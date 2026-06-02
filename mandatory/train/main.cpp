d45ce67d2c23d665adee5ad36a4deebe6c3ddaa0
Krugzer <154445387+MegaMogu95@users.noreply.github.com>
2026-06-02 16:05:11 +0000

    tout fonctionne

--- mandatory/train/main.cpp
+++ mandatory/train/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include "utils.hpp"
 
 int read_theta(double &theta0, double &theta1)
@@ -76,25 +77,45 @@ void    linear_reg_step(std::vector<double> &mileages, std::vector<double> &pric
     double  m = mileages.size();
 
     for (long unsigned int i = 0; i < m; i++)
+    {
         tmp0 += estimate_price(theta0, theta1, mileages[i]) - prices[i];
+        tmp1 += mileages[i] * (estimate_price(theta0, theta1, mileages[i]) - prices[i]);
+    }
     tmp0 *= rate / m;
-    for (long unsigned int i = 0; i < m; i++)
-        tmp1 += mileages[i] / m * (estimate_price(theta0, theta1, mileages[i]) - prices[i]);
-    tmp1 *= rate;
+    tmp1 *= rate / m;
     theta0 -= tmp0;
     theta1 -= tmp1;
 }
 
+void    normalize(std::vector<double> &v, double min, double max)
+{
+    for (size_t i = 0; i < v.size(); i++)
+        v[i] = (v[i] - min) / (max - min);
+}
+
+void    get_unnormalized_theta(double &theta0, double &theta1, double mileages_min, double mileages_max, double prices_min, double prices_max)
+{
+    theta1 *=(prices_max - prices_min) / (mileages_max - mileages_min);
+    theta0 = prices_min + theta0 * (prices_max - prices_min) - theta1 * mileages_min;
+}
+
 int main()
 {
-    double          theta0;
-    double          theta1;
+    double              theta0;
+    double              theta1;
     std::vector<double> mileages;
     std::vector<double> prices;
 
     read_theta(theta0, theta1);
     read_data(mileages, prices);
-    for (int i = 0; i < 1e7; i++)
-        linear_reg_step(mileages, prices, theta0, theta1, 1e-12);
+    double mileages_min = *min_element(mileages.begin(), mileages.end());
+    double mileages_max = *max_element(mileages.begin(), mileages.end());
+    double prices_min = *min_element(prices.begin(), prices.end());
+    double prices_max = *max_element(prices.begin(), prices.end());
+    normalize(mileages, mileages_min, mileages_max);
+    normalize(prices, prices_min, prices_max);
+    for (int i = 0; i < 10000; i++)
+        linear_reg_step(mileages, prices, theta0, theta1, 0.1);
+    get_unnormalized_theta(theta0, theta1, mileages_min, mileages_max, prices_min, prices_max);
     write_theta(theta0, theta1);
 }
